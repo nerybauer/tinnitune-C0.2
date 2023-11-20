f6d4b779b7842eb171b5e5f14f67f5e5ed0b0548
nerybauer <140842296+nerybauer@users.noreply.github.com>
2023-11-20 16:20:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-// Definindo as variáveis globais
+// Inicializa a variável do contexto de áudio fora do evento de clique para ter um escopo mais amplo
 let audioContext;
 let oscillator;
-let frequency = 440; // Valor padrão do slider
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Inicializando o contexto de áudio
-    audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-    // Manipulando o slider de frequência
+    // Obter referências para os elementos da página
     const frequencySlider = document.getElementById('frequencySlider');
+    const playSoundButton = document.getElementById('playSound');
+    const setFrequencyButton = document.getElementById('setFrequency');
+
+    // Evento de alteração do slider de frequência
     frequencySlider.addEventListener('input', function() {
-        frequency = this.value;
+        if (audioContext && oscillator) {
+            // Atualiza a frequência do oscillator em tempo real
+            oscillator.frequency.setValueAtTime(this.value, audioContext.currentTime);
+        }
     });
 
-    // Botão Play
-    const playSoundButton = document.getElementById('playSound');
-    playSoundButton.addEventListener('click', playSound);
+    // Evento de clique para tocar o som
+    playSoundButton.addEventListener('click', function() {
+        // Inicializa o contexto de áudio após a interação do usuário
+        if (!audioContext) {
+            audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        }
 
-    // Botão Esta é a minha frequência
-    const setFrequencyButton = document.getElementById('setFrequency');
-    setFrequencyButton.addEventListener('click', setFrequencyAndOpenNewPage);
+        // Retoma o contexto de áudio se necessário
+        if (audioContext.state === 'suspended') {
+            audioContext.resume();
+        }
+
+        // Chama a função para tocar o som
+        playSound(frequencySlider.value);
+    });
+
+    // Evento de clique para definir a frequência
+    setFrequencyButton.addEventListener('click', function() {
+        setFrequencyAndOpenNewPage(frequencySlider.value);
+    });
 });
 
-function playSound() {
-    // Implementação da função para tocar a onda senoidal
+function playSound(frequency) {
+    // Para o oscillator se ele já estiver tocando
+    if (oscillator) {
+        oscillator.stop();
+        oscillator.disconnect();
+    }
+    
+    // Cria um novo oscillator
+    oscillator = audioContext.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
 }
 
-function setFrequencyAndOpenNewPage() {
-    // Implementação da função para parar o som, armazenar a frequência e abrir uma nova página
+function setFrequencyAndOpenNewPage(frequency) {
+    // Para o som e desconecta o oscillator
+    if (oscillator) {
+        oscillator.stop();
+        oscillator.disconnect();
+    }
+    
+    // Armazena a frequência escolhida
+    localStorage.setItem('selectedFrequency', frequency);
+    
+    // Redireciona para a segunda página
+    window.location.href = 'second_page.html';
 }
