7dd83e129f4c4820966b1454da2788dba3466ff9
nerybauer <140842296+nerybauer@users.noreply.github.com>
2023-11-20 16:33:11 -0300

    Create second_script.js

---
+++ second_script.js
@@ -0,0 +1,62 @@
+let audioContext;
+let bandRejectFilter;
+let mp3Source;
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Verifica se o contexto de áudio já existe
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    
+    const storedFrequency = localStorage.getItem('selectedFrequency');
+    const playingFrequencyElement = document.getElementById('playingFrequency');
+    
+    if (storedFrequency) {
+        playingFrequencyElement.textContent = `Tocando na frequência de ${storedFrequency} Hz`;
+        startMp3WithBandRejectFilter(storedFrequency);
+    }
+
+    const stopMp3Button = document.getElementById('stopMp3');
+    stopMp3Button.addEventListener('click', stopMp3);
+
+    const learnMoreButton = document.getElementById('learnMore');
+    learnMoreButton.addEventListener('click', function() {
+        toggleConstructionPage(true);
+    });
+});
+
+function startMp3WithBandRejectFilter(frequency) {
+    // Aqui você irá implementar a lógica para tocar o MP3 e aplicar o filtro
+    // Por exemplo, você pode criar um elemento de áudio e aplicar o filtro ao seu source
+    const mp3Audio = new Audio('path_to_your_mp3_file.mp3'); // Substitua pelo caminho correto do seu arquivo
+    mp3Source = audioContext.createMediaElementSource(mp3Audio);
+    
+    bandRejectFilter = audioContext.createBiquadFilter();
+    bandRejectFilter.type = 'notch'; // Tipo 'notch' é um rejeita-faixa
+    bandRejectFilter.frequency.setValueAtTime(frequency, audioContext.currentTime);
+    bandRejectFilter.Q.value = 10; // Um valor Q alto significa uma rejeição mais estreita
+    
+    mp3Source.connect(bandRejectFilter);
+    bandRejectFilter.connect(audioContext.destination);
+    
+    mp3Audio.play();
+    animateSoundWaves(); // Função hipotética para iniciar a animação das ondas
+}
+
+function stopMp3() {
+    // Para de tocar o MP3 e desconecta os nós
+    if (mp3Source) {
+        mp3Source.mediaElement.pause();
+        mp3Source.disconnect();
+        bandRejectFilter.disconnect();
+    }
+}
+
+function toggleConstructionPage(show) {
+    // Mostra ou esconde a página de construção
+    const constructionPage = document.getElementById('constructionPage');
+    constructionPage.style.display = show ? 'block' : 'none';
+}
+
+// Implementação da função de animação das ondas senoidais
+function animateSoundWaves() {
+    // Esta função é apenas um placeholder. Você precisará implementar a animação das ondas senoidais.
+}
